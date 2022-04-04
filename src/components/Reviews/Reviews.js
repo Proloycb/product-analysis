54b01cbb568ec11f6e026f55f5119dc1bc2fb155
Proloycb <proloychacroborty@gmail.com>
2022-04-04 19:22:25 +0600

    review page completed

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import useReviews from '../../hooks/useReviews';
+import Card from '../Card/Card';
 
 const Reviews = () => {
+    const [reviews] = useReviews();
     return (
-        <div>
-            
+        <div className='grid grid-cols-3 gap-8 md:m-14'>
+            {
+                reviews.map(review => <Card
+                    key={review.id}
+                    review={review}
+                ></Card>)
+            }
         </div>
     );
 };
